588109d575c966b667625769dcf13c75a064d9a8
ToWorld <jayzhu1215@gmail.com>
2019-06-12 15:47:30 +0800

    style fix

--- contractsdk/cpp/reserved/banned.cc
+++ contractsdk/cpp/reserved/banned.cc
@@ -7,31 +7,31 @@ class Banned : public xchain::Contract {};
 const char delimiter = ',';
 
 const std::vector<std::string> split(const std::string& rawStr) {
-	std::vector<std::string> res;
-	if (rawStr == "") {
-		return res;
-	}
-	int i = 0;
-	for (; i < rawStr.size(); ++i) {
-		if (rawStr[i] == delimiter) {
-			continue;
+    std::vector<std::string> res;
+    if (rawStr == "") {
+        return res;
+    }
+    int i = 0;
+    for (; i < rawStr.size(); ++i) {
+        if (rawStr[i] == delimiter) {
+            continue;
 		}
-		break;
-	}
-	if (i >= rawStr.size()) {
-		return res;
-	}
-	std::string delimStr = std::string(1, delimiter);
-	std::string str = rawStr.substr(i) + delimStr;
-	size_t pos = std::string::npos;
-	while ((pos=str.find(delimStr)) != std::string::npos) {
-		std::string temp = str.substr(0, pos);
-		if (temp != "") {
-			res.push_back(temp);
-		}
-		str = str.substr(pos+1,str.size());
-	}
-	return res;
+        break;
+    }
+    if (i >= rawStr.size()) {
+        return res;
+    }
+    std::string delimStr = std::string(1, delimiter);
+    std::string str = rawStr.substr(i) + delimStr;
+    size_t pos = std::string::npos;
+    while ((pos=str.find(delimStr)) != std::string::npos) {
+        std::string temp = str.substr(0, pos);
+        if (temp != "") {
+            res.push_back(temp);
+        }
+        str = str.substr(pos+1,str.size());
+    }
+    return res;
 }
 
 DEFINE_METHOD(Banned, initialize) {
@@ -44,14 +44,14 @@ DEFINE_METHOD(Banned, ban) {
     const std::string keys = ctx->arg("contract");
     const std::string value = "true";
 
-	const std::vector<std::string> contracts = split(keys);
+    const std::vector<std::string> contracts = split(keys);
 
-	for (auto iter = contracts.begin(); iter != contracts.end(); ++iter) {
-    	bool ret = ctx->put_object(*iter, value);
-		if (!ret) {
-			ctx->error("ban failed");
-			return;
-		}
+    for (auto iter = contracts.begin(); iter != contracts.end(); ++iter) {
+        bool ret = ctx->put_object(*iter, value);
+        if (!ret) {
+            ctx->error("ban failed");
+            return;
+        }
     }
     ctx->ok("ban contract success");
 }
@@ -60,15 +60,15 @@ DEFINE_METHOD(Banned, release) {
     xchain::Context* ctx = self.context();
     const std::string keys = ctx->arg("contract");
 
-	const std::vector<std::string> contracts = split(keys);
-	for (auto iter = contracts.begin(); iter != contracts.end(); ++iter) {
-		bool ret = ctx->delete_object(*iter);
-		if (!ret) {
-			ctx->error("release failed");
-			return;
-		}
-	}
-	ctx->ok("release contract success");
+    const std::vector<std::string> contracts = split(keys);
+    for (auto iter = contracts.begin(); iter != contracts.end(); ++iter) {
+        bool ret = ctx->delete_object(*iter);
+        if (!ret) {
+            ctx->error("release failed");
+            return;
+        }
+    }
+    ctx->ok("release contract success");
 }
 
 DEFINE_METHOD(Banned, get) {
