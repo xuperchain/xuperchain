5bd19d3e12630684e925e30e95d9168025b0591e
fxsjy <sunjunyi01@baidu.com>
2019-06-14 21:08:43 +0800

    Automatic rollback of transactions that have not been confirmed for too long

--- common/config/config.go
+++ common/config/config.go
@@ -98,6 +98,7 @@ type UtxoConfig struct {
 	ContractWhiteList     map[string]map[string]bool `yaml:"contractWhiteList,omitempty"`
 	// 是否开启新版本tx k = bcname, v = isBetaTx
 	IsBetaTx map[string]bool `yaml:"isBetaTx,omitempty"`
+	MaxConfirmedDelay     uint32 `yaml:"maxConfirmedDelay,omitempty"`
 }
 
 // FeeConfig is the config of Fee
@@ -241,6 +242,7 @@ func (nc *NodeConfig) defaultNodeConfig() {
 		ContractExecutionTime: 500,
 		ContractWhiteList:     make(map[string]map[string]bool),
 		IsBetaTx:              make(map[string]bool),
+		MaxConfirmedDelay:     300,
 	}
 	nc.DedupCacheSize = 50000
 	nc.Kernel = KernelConfig{
