807ed93fa2adee662e9a165a6433ee35f54c7158
qizheng09 <qizheng09@outlook.com>
2019-06-17 15:05:21 +0800

    add identity contract

--- contractsdk/cpp/reserved/identity.cc
+++ contractsdk/cpp/reserved/identity.cc
@@ -7,82 +7,83 @@ class Identity : public xchain::Contract {};
 const char delimiter = ',';
 
 void split_str(const std::string& aks, std::vector<std::string>& ak_sets) {
-  std::string sub_str = std::string(1, delimiter);
-  std::string::size_type pos1, pos2;
-  pos2 = aks.find(sub_str);
-  pos1 = 0;
-  while(std::string::npos != pos2)
-    {
-      ak_sets.push_back(aks.substr(pos1, pos2-pos1));
-      pos1 = pos2 + sub_str.size();
-      pos2 = aks.find(sub_str, pos1);
+    std::string sub_str = std::string(1, delimiter);
+    std::string::size_type pos1, pos2;
+    pos2 = aks.find(sub_str);
+    pos1 = 0;
+    while(std::string::npos != pos2) {
+        ak_sets.push_back(aks.substr(pos1, pos2-pos1));
+        pos1 = pos2 + sub_str.size();
+        pos2 = aks.find(sub_str, pos1);
     }
-  if(pos1 != aks.length())
-    ak_sets.push_back(aks.substr(pos1));
+    if(pos1 != aks.length()) {
+        ak_sets.push_back(aks.substr(pos1));
+    }
 }
 
 DEFINE_METHOD(Identity, initialize) {
-  xchain::Context* ctx = self.context();
-  ctx->ok("initialize identity contract success");
+    xchain::Context* ctx = self.context();
+    ctx->ok("initialize identity contract success");
 }
 
 // register method register aks to identify contract
 DEFINE_METHOD(Identity, register_aks) {
-  xchain::Context* ctx = self.context();
+    xchain::Context* ctx = self.context();
 
-  // aks register to identity contract
-  const std::string aks = ctx->arg("aks");
-  std::vector<std::string> ak_sets;
-  split_str(aks, ak_sets);
+    // aks register to identity contract
+    const std::string aks = ctx->arg("aks");
+    std::vector<std::string> ak_sets;
+    split_str(aks, ak_sets);
 
-  for(auto iter = ak_sets.begin(); iter != ak_sets.end(); iter++) {
-    if (!ctx->put_object(*iter, "true")) {
-      ctx->error("register aks to identify contract error");
-      return;
+    for(auto iter = ak_sets.begin(); iter != ak_sets.end(); iter++) {
+        if (!ctx->put_object(*iter, "true")) {
+            ctx->error("register aks to identify contract error");
+            return;
+        }
     }
-  }
-  ctx->ok("register aks to identify contract success");
+
+    ctx->ok("register aks to identify contract success");
 }
 
 // unregister method unregister aks from identify contract
 DEFINE_METHOD(Identity, unregister_aks) {
-  xchain::Context* ctx = self.context();
+    xchain::Context* ctx = self.context();
 
-  // aks unregister form identity contract
-  const std::string aks = ctx->arg("aks");
-  std::vector<std::string> ak_sets;
-  split_str(aks, ak_sets);
-
-  for (auto iter = ak_sets.begin(); iter != ak_sets.end(); iter++) {
-    if (!ctx->delete_object(*iter)) {
-        ctx->error("unregister from identify contract error");
-        return;
+    // aks unregister form identity contract
+    const std::string aks = ctx->arg("aks");
+    std::vector<std::string> ak_sets;
+    split_str(aks, ak_sets);
+  
+    for (auto iter = ak_sets.begin(); iter != ak_sets.end(); iter++) {
+        if (!ctx->delete_object(*iter)) {
+            ctx->error("unregister from identify contract error");
+            return;
+        }
     }
-  }
 
-  ctx->ok("unregister aks from identify contract success");
+    ctx->ok("unregister aks from identify contract success");
 }
 
 // verify method verify whether the aks were identified
 DEFINE_METHOD(Identity, verify) {
-  xchain::Context* ctx = self.context();
-  std::string value;
-  // FIXME zq: @icexin contex need to support initiator
-  const std::string initiator;
-  ctx->get_object(initiator, &value);
-  if (value != "true") {
-    ctx->error("verify initiator error");
-    return;
-  }
-
-  // FIXME zq: @icexin contex need to support auth_require
-  std::vector<std::string> auth_require;
-  for (auto iter = auth_require.begin(); iter != auth_require.end(); iter++) {
-    ctx->get_object(*iter, &value);
+    xchain::Context* ctx = self.context();
+    std::string value;
+    // FIXME zq: @icexin contex need to support initiator
+    const std::string initiator;
+    ctx->get_object(initiator, &value);
     if (value != "true") {
-      ctx->error("verify auth_require error");
-      return;
+        ctx->error("verify initiator error");
+        return;
     }
-  }
-  ctx-> ok("verify all aks success");
+
+    // FIXME zq: @icexin contex need to support auth_require
+    std::vector<std::string> auth_require;
+    for (auto iter = auth_require.begin(); iter != auth_require.end(); iter++) {
+        ctx->get_object(*iter, &value);
+        if (value != "true") {
+            ctx->error("verify auth_require error");
+            return;
+        }
+    }
+    ctx-> ok("verify all aks success");
 }
