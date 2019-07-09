aa27dfc966c19458d3c980b9728bf59b7c9e2a92
qizheng09 <qizheng09@outlook.com>
2019-07-09 17:20:17 +0800

    fix reserved contracts

--- utxo/reserved_contract.go
+++ utxo/reserved_contract.go
@@ -56,11 +56,9 @@ func (uv *UtxoVM) getReservedContractRequests(req []*pb.InvokeRequest, isPreExec
 		if len(req) < len(reservedContracts) {
 			uv.xlog.Warn("req should contain reservedContracts")
 			return nil, ErrGetReservedContracts
-		} else if len(req) == len(reservedContracts) {
-			// req only contain reservedContracts
-			return reservedContracts, nil
+		} else if len(req) < len(reservedContracts) {
+			ra = genArgs(req[len(reservedContracts):])
 		}
-		ra = genArgs(req[len(reservedContracts):])
 	}
 
 	for _, rc := range reservedContracts {
