a7db9f20649b5be2eeb7fce2b0a868a0be9e2973
Miao Zhang <zhangdaxiaomiao@163.com>
2019-07-16 23:46:33 -0500

    Add Logid in message for verify function (#217)

--- p2pv2/subscriber.go
+++ p2pv2/subscriber.go
@@ -44,7 +44,7 @@ func (sub *Subscriber) handleMessage(s *Stream, msg *xuperp2p.XuperMessage) {
 		if s.node.srv.config.IsAuthentication && !s.auth() {
 			s.node.log.Trace("Stream not authenticated")
 			resType := xuperp2p.GetResMsgType(msg.GetHeader().GetType())
-			res, _ := xuperp2p.NewXuperMessage(xuperp2p.XuperMsgVersion2, "", "",
+			res, _ := xuperp2p.NewXuperMessage(xuperp2p.XuperMsgVersion2, "", msg.GetHeader().GetLogid(),
 				resType, []byte(""), xuperp2p.XuperMessage_GET_AUTHENTICATION_NOT_PASS)
 			if err := s.writeData(res); err != nil {
 				fmt.Println("Stream not authenticated to write msg error", "err", err)
