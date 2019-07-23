8c3ec855506cd1d2e574828522d3f23da7ac7547
Bing Duan <hibduan@gmail.com>
2019-07-23 21:08:21 +0800

    XuperCDT: add table engine and basic test framework (#224)
    * XuperCDT: add table engine and basic test framework
    
    * XuperCDT:
    1. expose Iterator, not range query and fix some bugs
    2. Iterator::_cur_elem be covered if it points to last one in _buf
    3. scan signature modiffying
    
    * XuperCDT:
    1. remove regex lib to decrease size of wasm
    2. remove unused headers
    
    * XuperCDT:
    
    1. use unique_ptr to erase copy-constructor
    2. remove copy constructor
    
    * XuperCDT: change opt level back to Oz

--- contractsdk/cpp/build.sh
+++ contractsdk/cpp/build.sh
@@ -2,4 +2,13 @@
 
 # install docker in precondition
 
-docker run -u $UID --rm -v $(pwd):/src hub.baidubce.com/xchain/emcc emmake make
+function test() {
+    #docker run -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'protoc -I/source/pb /source/pb/anchor.proto --cpp_out=/source/pb/'
+    docker run -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'cd /source && make clean && make test'
+}
+
+function build() {
+    docker run -u $UID --rm -v $(pwd):/src hub.baidubce.com/xchain/emcc emmake make 
+}
+
+if [[ -z "$1" ]]; then build; else $1; fi
