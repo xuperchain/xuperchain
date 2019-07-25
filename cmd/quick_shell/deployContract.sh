8732f9cfb4126200baec3220778e353a12a8a0d4
ToWorld <jayzhu1215@gmail.com>
2019-07-25 19:59:39 +0800

    update deployContract parameter

--- cmd/quick_shell/deployContract.sh
+++ cmd/quick_shell/deployContract.sh
@@ -2,13 +2,14 @@
 set -e
 # check input parameter
 if [ $# -lt 4 ]; then
-	echo "usage: sh -x deployContract.sh accountName contractName contractNamePath fee"
+	echo "usage: sh -x deployContract.sh accountName contractName contractNamePath args fee"
 	exit
 fi
 accountName=$1
 contractName=$2
 contractNamePath=$3
-fee=$4
+args=$4
+fee=$5
 address=`cat ./data/keys/address`
 # get testnet conf including ip:port and complianceCheck public key
 source "./testnet.conf"
@@ -22,7 +23,7 @@ if [ ! -f "$addrs" ]; then
 fi
 echo $public_key > data/acl/addrs
 echo "$accountName/$address" >> data/acl/addrs
-./xchain-cli wasm deploy --account $accountName --cname $contractName -H $ip_port -m $contractNamePath --arg '{"creator":"xchain"}' --output contractRawTx.out --fee $fee
+./xchain-cli wasm deploy --account $accountName --cname $contractName -H $ip_port -m $contractNamePath --arg $args --output contractRawTx.out --fee $fee
 ./xchain-cli multisig get --tx ./contractRawTx.out --host $ip_port --output complianceCheck.out
 ./xchain-cli multisig sign --tx ./contractRawTx.out --output my.sign
 ./xchain-cli multisig send my.sign complianceCheck.out,my.sign --tx ./contractRawTx.out -H $ip_port
