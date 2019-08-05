6f00e699d5b9f8e5ad6237f94c8f122cd60bea58
ToWorld <jayzhu1215@gmail.com>
2019-08-05 15:49:22 +0800

    check the final sequence generated

--- utxo/topsort_test.go
+++ utxo/topsort_test.go
@@ -16,6 +16,10 @@ func TestTopSortWithCircle(t *testing.T) {
 }
 
 func TestTopSortWithoutCircle(t *testing.T) {
+	ret1 := "tx3tx1tx2"
+	ret2 := "tx3tx2tx1"
+	ret3 := "tx4tx5tx6"
+	ret4 := "tx4tx6tx5"
 	graph := map[string][]string{}
 	graph["tx3"] = []string{"tx1", "tx2"}
 	graph["tx4"] = []string{"tx5", "tx6"}
@@ -23,15 +27,13 @@ func TestTopSortWithoutCircle(t *testing.T) {
 	if circle != nil || len(order) != 6 {
 		t.Error("TestTopSortWithoutCircle error")
 	}
-}
-
-func TestTopSortWithIsolatedNetwork(t *testing.T) {
-	graph := map[string][]string{}
-	graph["tx3"] = []string{"tx1", "tx2"}
-	graph["tx4"] = []string{"tx5", "tx6"}
-	graph["tx7"] = []string{}
-	order, circle := TopSortDFS(graph)
-	if circle != nil || len(order) != 7 {
-		t.Error("TestTopSortWithIsolatedNetwork error")
+	// tx3 tx1 tx2 | tx2 tx1
+	// tx4 tx5 tx6 | tx6 tx5
+	orderStr := ""
+	for _, str := range order {
+		orderStr += str
+	}
+	if orderStr != (ret1+ret3) && orderStr != (ret1+ret4) && orderStr != (ret2+ret3) && orderStr != (ret2+ret4) && orderStr != (ret3+ret1) && orderStr != (ret3+ret2) && orderStr != (ret4+ret1) && orderStr != (ret4+ret2) {
+		t.Error("TestTopSortWithoutCircle error")
 	}
 }
