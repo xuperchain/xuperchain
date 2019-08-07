163f1874c9f4ff004ecc621572122fa4b01b0862
MichelleZ <zhangdaxiaomiao@163.com>
2019-08-07 11:37:37 +0800

    Use directly

--- contractsdk/cpp/build.sh
+++ contractsdk/cpp/build.sh
@@ -3,17 +3,11 @@
 # install docker in precondition
 
 function test() {
-    verify
-    docker run -u $UID -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'cd /source && make clean && make test'
+    docker run -u `id -u` -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'cd /source && make clean && make test'
 }
 
 function build() {
-    verify
-    docker run -u $UID --rm -v $(pwd):/src hub.baidubce.com/xchain/emcc emmake make 
-}
-
-function verify {
-    [ ! `id -u` ] && echo "$UID not exist" && exit 1
+    docker run -u `id -u` --rm -v $(pwd):/src hub.baidubce.com/xchain/emcc emmake make 
 }
 
 if [[ -z "$1" ]]; then build; else $1; fi
