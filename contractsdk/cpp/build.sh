e574438d1eb7b038fed101f421ec101b94cd2a35
Miao Zhang <zhangdaxiaomiao@163.com>
2019-08-09 17:26:57 +0800

    add test case for built-in types (#259)
    * add test case for built-in types
    
    * add UID judgement logic
    
    * Use directly
    
    * use PRId64
    
    * remove compilation params without affecting the overall situation

--- contractsdk/cpp/build.sh
+++ contractsdk/cpp/build.sh
@@ -3,12 +3,11 @@
 # install docker in precondition
 
 function test() {
-    #docker run -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'protoc -I/source/pb /source/pb/anchor.proto --cpp_out=/source/pb/'
-    docker run -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'cd /source && make clean && make test'
+    docker run -u `id -u` -it --rm -v $PWD:/source hub.baidubce.com/xch/contract-dev sh -c 'cd /source && make clean && make test'
 }
 
 function build() {
-    docker run -u $UID --rm -v $(pwd):/src hub.baidubce.com/xchain/emcc emmake make 
+    docker run -u `id -u` --rm -v $(pwd):/src hub.baidubce.com/xchain/emcc emmake make 
 }
 
 if [[ -z "$1" ]]; then build; else $1; fi
