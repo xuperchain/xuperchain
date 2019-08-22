6234afd396d5ed84fa72e6b00c8ed29a87c28d05
ZhengQi <qizheng09@outlook.com>
2019-08-22 10:39:53 +0800

    Add safety rules for Chained-bft.  (#320)
    * add reserved whitelist mechanism
    
    * remove unused log
    
    * add chainedbft framework
    
    * add p2p messages
    
    * add process newview
    
    * add process new proposal
    
    * add all handler except TODOs
    
    * add external consensus interface
    
    * implement all TODOs
    
    * add new instance params
    
    * add GetGenerateQC interface
    
    * add safety rules

--- consensus/common/chainedbft/smr/type.go
+++ consensus/common/chainedbft/smr/type.go
@@ -2,7 +2,6 @@ package smr
 
 import (
 	"crypto/ecdsa"
-	"errors"
 	"sync"
 
 	log "github.com/xuperchain/log15"
@@ -10,7 +9,6 @@ import (
 	"github.com/xuperchain/xuperunion/consensus/common/chainedbft/config"
 	"github.com/xuperchain/xuperunion/consensus/common/chainedbft/external"
 	chainedbft_pb "github.com/xuperchain/xuperunion/consensus/common/chainedbft/pb"
-	"github.com/xuperchain/xuperunion/consensus/common/chainedbft/utils"
 	crypto_base "github.com/xuperchain/xuperunion/crypto/client/base"
 	"github.com/xuperchain/xuperunion/p2pv2"
 	xuper_p2p "github.com/xuperchain/xuperunion/p2pv2/pb"
@@ -55,107 +53,4 @@ type Smr struct {
 
 	// quitCh stop channel
 	QuitCh chan bool
-}
-
-// addViewMsg check and add new view msg to smr
-// 1: check sign of msg
-// 2: check if the msg from validate sets replica
-func (s *Smr) addViewMsg(msg *chainedbft_pb.ChainedBftPhaseMessage) error {
-	// check msg sign
-	ok, err := utils.VerifyPhaseMsgSign(s.cryptoClient, msg)
-	if !ok || err != nil {
-		s.slog.Error("addViewMsg VerifyPhaseMsgSign error", "ok", ok, "error", err)
-		return errors.New("addViewMsg VerifyPhaseMsgSign error")
-	}
-	// check whether view outdate
-	if msg.GetViewNumber() < s.votedView {
-		s.slog.Error("addViewMsg view outdate", "votedView", s.votedView, "viewRecivied", msg.GetViewNumber())
-		return errors.New("addViewMsg view outdate")
-	}
-
-	// check in ValidateSets
-	if !utils.IsInValidateSets(s.validates, msg.GetSignature().GetAddress()) {
-		s.slog.Error("addViewMsg checkValidateSets error")
-		return errors.New("addViewMsg checkValidateSets error")
-	}
-	// add JustifyQC
-	if msg.GetJustifyQC() != nil {
-		s.slog.Info("addViewMsg GetJustifyQC not nil", "GetJustifyQC.SignInfos", msg.GetJustifyQC().GetSignInfos())
-		s.qcVoteMsgs.LoadOrStore(string(msg.GetJustifyQC().GetProposalId()), msg.GetJustifyQC().GetSignInfos())
-	}
-
-	// add View msg
-	v, ok := s.newViewMsgs.Load(msg.GetViewNumber())
-	if !ok {
-		viewMsgs := []*chainedbft_pb.ChainedBftPhaseMessage{}
-		viewMsgs = append(viewMsgs, msg)
-		s.newViewMsgs.Store(msg.GetViewNumber(), viewMsgs)
-		return nil
-	}
-
-	viewMsgs := v.([]*chainedbft_pb.ChainedBftPhaseMessage)
-	viewMsgs = append(viewMsgs, msg)
-	s.newViewMsgs.Store(msg.GetViewNumber(), viewMsgs)
-	return nil
-}
-
-// addVoteMsg check and add vote msg to smr
-// 1: check sign of msg
-// 2: check if the msg from validate sets
-func (s *Smr) addVoteMsg(msg *chainedbft_pb.ChainedBftVoteMessage) error {
-	// check msg sign
-	proposalMsg, err := s.externalCons.CallProposalMsgWithProposalID(msg.GetProposalId())
-	if err != nil {
-		s.slog.Error("addVoteMsg CallProposalMsgWithProposalID error", "error", err)
-		return err
-	}
-	ok, err := utils.VerifyVoteMsgSign(s.cryptoClient, msg.GetSignature(), proposalMsg)
-	if !ok || err != nil {
-		s.slog.Error("addVoteMsg VerifyVoteMsgSign error", "ok", ok, "error", err)
-		return errors.New("addVoteMsg VerifyVoteMsgSign error")
-	}
-	// check in ValidateSets
-	if !utils.IsInValidateSets(s.validates, msg.GetSignature().GetAddress()) {
-		s.slog.Error("addVoteMsg IsInValidateSets error")
-		return errors.New("addVoteMsg IsInValidateSets error")
-	}
-
-	// add vote msg
-	v, ok := s.qcVoteMsgs.Load(string(msg.GetProposalId()))
-	if !ok {
-		voteMsgs := &chainedbft_pb.QCSignInfos{}
-		voteMsgs.QCSignInfos = append(voteMsgs.QCSignInfos, msg.GetSignature())
-		s.qcVoteMsgs.Store(string(msg.GetProposalId()), voteMsgs)
-		return nil
-	}
-
-	voteMsgs := v.(*chainedbft_pb.QCSignInfos)
-	if utils.CheckIsVoted(voteMsgs, msg.GetSignature()) {
-		s.slog.Error("addVoteMsg CheckIsVoted error, this address have voted")
-		return errors.New("addVoteMsg CheckIsVoted error")
-	}
-	voteMsgs.QCSignInfos = append(voteMsgs.QCSignInfos, msg.GetSignature())
-	s.qcVoteMsgs.Store(string(msg.GetProposalId()), voteMsgs)
-	return nil
-}
-
-// checkVoteNum leader will check whether the vote nums more than (n-f)
-func (s *Smr) checkVoteNum(proposalID []byte) bool {
-	v, ok := s.qcVoteMsgs.Load(string(proposalID))
-	if !ok {
-		s.slog.Error("smr checkVoteNum error, voteMsgs not found!")
-		return false
-	}
-	voteMsgs := v.(*chainedbft_pb.QCSignInfos)
-
-	if len(voteMsgs.GetQCSignInfos()) > (len(s.validates)-1)*2/3 {
-		return true
-	}
-	return false
-}
-
-// UpdateValidateSets update current ValidateSets by ex
-func (s *Smr) UpdateValidateSets(validates []*cons_base.CandidateInfo) error {
-	s.validates = validates
-	return nil
 }
