539ebad828d359bff8e11f6f4d670b2309e19ea2
Yucao Wang <yucwang@outlook.com>
2019-08-22 02:17:49 -0500

    [Feature] Support MultiSignature in default/schnorr crypto client (#306)

--- cmd/cli/multisig_gen.go
+++ cmd/cli/multisig_gen.go
@@ -5,10 +5,16 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"crypto/ecdsa"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/spf13/cobra"
+	crypto_client "github.com/xuperchain/xuperunion/crypto/client"
 	"github.com/xuperchain/xuperunion/utxo"
 )
 
@@ -25,7 +31,9 @@ type MultisigGenCommand struct {
 	version      int32
 	output       string
 	multiAddrs   string
+	pubkeys      string
 	from         string
+	signType     string
 	// contract params
 	moduleName   string
 	contractName string
@@ -58,6 +66,8 @@ func (c *MultisigGenCommand) addFlags() {
 	c.cmd.Flags().Int32Var(&c.version, "txversion", utxo.TxVersion, "Tx version.")
 	c.cmd.Flags().StringVarP(&c.output, "output", "o", "./tx.out", "Serialized transaction data file.")
 	c.cmd.Flags().StringVarP(&c.multiAddrs, "multiAddrs", "A", "data/acl/addrs", "MultiAddrs to fill required accounts/addresses.")
+	c.cmd.Flags().StringVarP(&c.pubkeys, "publickeys", "P", "data/acl/pubkeys", "public keys of initiator and auth_require addresses.")
+	c.cmd.Flags().StringVar(&c.signType, "signtype", "", "type of signature, support multi/ring")
 	c.cmd.Flags().StringVar(&c.from, "from", "", "Initiator of an transaction.")
 	c.cmd.Flags().StringVar(&c.moduleName, "module", "", "Contract type: xkernel or wasm or native, native is deprecated.")
 	c.cmd.Flags().StringVar(&c.contractName, "contract", "", "Contract name to be called.")
@@ -67,6 +77,23 @@ func (c *MultisigGenCommand) addFlags() {
 
 // gen 命令入口
 func (c *MultisigGenCommand) generateTx(ctx context.Context) error {
+	var msd *MultisigData
+	if c.signType == "multi" {
+		// multisig, use XuperSign
+		pubkeys, err := c.readPublicKeysFromFile(c.pubkeys)
+		if err != nil {
+			return err
+		}
+
+		msd, err = c.getMultiSignData(pubkeys)
+		if err != nil {
+			return err
+		}
+	} else if c.signType != "" {
+		fmt.Printf("SignType[%s] is not supported", c.signType)
+		return fmt.Errorf("SignType is not supported")
+	}
+
 	ct := &CommTrans{
 		To:           c.to,
 		Amount:       c.amount,
@@ -96,5 +123,79 @@ func (c *MultisigGenCommand) generateTx(ctx context.Context) error {
 		}
 	}
 
+	if msd != nil {
+		jsonData, err := json.Marshal(msd)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(c.output+".ext", jsonData, 0755)
+		if err != nil {
+			return fmt.Errorf("write file error")
+		}
+	}
+
 	return ct.GenerateMultisigGenRawTx(ctx)
+}
+
+func (c *MultisigGenCommand) readPublicKeysFromFile(filename string) ([][]byte, error) {
+	var pubkeys [][]byte
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		pubkeys = append(pubkeys, line)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Scan file failed, error=%v", err)
+		return nil, err
+	}
+	return pubkeys, nil
+}
+
+func (c *MultisigGenCommand) getMultiSignData(pubkeys [][]byte) (*MultisigData, error) {
+	var klist [][]byte
+	var rlist [][]byte
+	var pklist []*ecdsa.PublicKey
+	if len(pubkeys) < 2 {
+		fmt.Println("the number of public keys for multisig should more than 2")
+		return nil, fmt.Errorf("invalid public keys")
+	}
+
+	xcc, err := crypto_client.CreateCryptoClientFromJSONPublicKey(pubkeys[0])
+	if err != nil {
+		return nil, err
+	}
+
+	for _, pubkey := range pubkeys {
+		// get Ki
+		ki, err := xcc.GetRandom32Bytes()
+		if err != nil {
+			return nil, err
+		}
+		klist = append(klist, ki)
+		pki, err := xcc.GetEcdsaPublicKeyFromJSON(pubkey)
+		if err != nil {
+			return nil, err
+		}
+		pklist = append(pklist, pki)
+		ri := xcc.GetRiUsingRandomBytes(pki, ki)
+		rlist = append(rlist, ri)
+	}
+	r := xcc.GetRUsingAllRi(pklist[0], rlist)
+	spk, err := xcc.GetSharedPublicKeyForPublicKeys(pklist)
+	if err != nil {
+		return nil, err
+	}
+
+	msd := &MultisigData{
+		R:       r,
+		C:       spk,
+		KList:   klist,
+		PubKeys: pubkeys,
+	}
+	return msd, nil
 }
