d52e3567e3ad3544538d247fab00df649f8a61af
ZhengQi <qizheng09@outlook.com>
2019-08-22 10:39:53 +0800

    Add safety rules for Chained-bft.  (#320)
    * add reserved whitelist mechanism
    
    * remove unused log
    
    * add chainedbft framework
    
    * add p2p messages
    
    * add process newview
    
    * add process new proposal
    
    * add all handler except TODOs
    
    * add external consensus interface
    
    * implement all TODOs
    
    * add new instance params
    
    * add GetGenerateQC interface
    
    * add safety rules

--- consensus/common/chainedbft/smr/safety_rules.go
+++ consensus/common/chainedbft/smr/safety_rules.go
@@ -1,11 +1,42 @@
 package smr
 
-import chainedbft_pb "github.com/xuperchain/xuperunion/consensus/common/chainedbft/pb"
+import (
+	chainedbft_pb "github.com/xuperchain/xuperunion/consensus/common/chainedbft/pb"
+	"github.com/xuperchain/xuperunion/consensus/common/chainedbft/utils"
+)
 
 // safeProposal make sure whether Proposal is safe
-// 1 smr check whether the proposal is true
-// 2 external consensus make sure whether the proposalMsg is safe
-// TODO: zq
-func (s *Smr) safeProposal(propsQC, prePropsQC *chainedbft_pb.QuorumCert) (bool, error) {
-	return false, nil
+// 1 check the proposal' view number
+// 2 verify justify's votes
+// 3 external consensus make sure whether the proposalMsg is safe
+func (s *Smr) safeProposal(propsQC, justify *chainedbft_pb.QuorumCert) (bool, error) {
+	// step1: liveness rule
+	// the proposQC's view number need to more than lockedQC
+	if propsQC.GetViewNumber() < s.lockedQC.GetViewNumber() {
+		s.slog.Error("safeProposal liveness rule error",
+			"propsQC.ViewNum", propsQC.GetViewNumber(), "lockedQC.ViewNum", s.lockedQC.GetViewNumber())
+		return false, ErrPropsViewNum
+	}
+	// step2: verify justify's votes
+	// verify justify sign number
+	justifySigns := justify.GetSignInfos().GetQCSignInfos()
+	if len(justifySigns) <= (len(s.validates)-1)*2/3 {
+		s.slog.Error("safeProposal proposal justify sign not enough error")
+		return false, ErrJustifySignNotEnough
+	}
+	// verify justify sign
+	for _, v := range justifySigns {
+		if !utils.IsInValidateSets(s.validates, v.GetAddress()) {
+			s.slog.Error("safeProposal IsInValidateSets error")
+			return false, ErrInValidateSets
+		}
+
+		ok, err := utils.VerifyVoteMsgSign(s.cryptoClient, v, justify.GetProposalId())
+		if !ok || err != nil {
+			s.slog.Error("safeProposal VerifyVoteMsgSign error", "ok", ok, "error", err)
+			return false, ErrVerifyVoteSign
+		}
+	}
+	// step3: call external consensus verify proposalMsg
+	return s.externalCons.CallVerifyQc(propsQC)
 }
