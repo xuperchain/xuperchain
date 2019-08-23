a43fe2b30c04dc681452e8f4ca9feccccf7c8d44
ZhengQi <qizheng09@outlook.com>
2019-08-23 13:43:06 +0800

    Add QuorumCert in Block struct and add some unit test for chained-bft. (#325)
    * add QC in block struct and add some unit test for chained-bft

--- build.sh
+++ build.sh
@@ -10,14 +10,12 @@ cd `dirname $0`
 # go install github.com/golang/protobuf/protoc-gen-go
 # go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 
-protoc -I pb pb/xchain.proto pb/xchain_spv.proto pb/xcheck.proto \
+protoc -I pb pb/xchain.proto pb/xchain_spv.proto pb/xcheck.proto pb/chainedbft.proto \
 	-I pb/googleapis \
 	--go_out=plugins=grpc:pb \
-	--grpc-gateway_out=logtostderr=true:pb
+	--grpc-gateway_out=logtostderr=true:pb 
 
 protoc -I p2pv2/pb p2pv2/pb/message.proto  --go_out=p2pv2/pb
-
-protoc -I consensus/common/chainedbft/pb consensus/common/chainedbft/pb/chainedbft.proto --go_out=consensus/common/chainedbft/pb 
 
 protoc -I xmodel/pb xmodel/pb/versioned_data.proto --go_out=xmodel/pb 
 
--- consensus/common/chainedbft/utils/utils.go
+++ consensus/common/chainedbft/utils/utils.go
@@ -2,7 +2,7 @@ package utils
 
 import (
 	cons_base "github.com/xuperchain/xuperunion/consensus/base"
-	chainedbft_pb "github.com/xuperchain/xuperunion/consensus/common/chainedbft/pb"
+	pb "github.com/xuperchain/xuperunion/pb"
 )
 
 // IsInValidateSets check whether addr in validates
@@ -16,7 +16,7 @@ func IsInValidateSets(validates []*cons_base.CandidateInfo, addr string) bool {
 }
 
 // CheckIsVoted return whether the address have voted for this QC
-func CheckIsVoted(votedMsgs *chainedbft_pb.QCSignInfos, voteMsg *chainedbft_pb.SignInfo) bool {
+func CheckIsVoted(votedMsgs *pb.QCSignInfos, voteMsg *pb.SignInfo) bool {
 	for _, v := range votedMsgs.QCSignInfos {
 		if v.GetAddress() == voteMsg.GetAddress() {
 			return true
