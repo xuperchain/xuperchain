e85aa9cab35aeee10b5986a9be4c02de62f1bdfb
xuyang05 <xuyang05@baidu.com>
2019-09-19 21:02:36 +0800

    for release build

--- build.sh
+++ build.sh
@@ -35,8 +35,10 @@ function buildpkg() {
     output=$1
     pkg=$2
     buildVersion=`git rev-parse --abbrev-ref HEAD`
+	buildVersion=${buildVersion:-"release"}
     buildDate=$(date "+%Y-%m-%d-%H:%M:%S")
     commitHash=`git rev-parse --short HEAD`
+	commitHash=${commitHash:-"stable"}
     go build -o $output -ldflags "-X main.buildVersion=$buildVersion -X main.buildDate=$buildDate -X main.commitHash=$commitHash" $pkg
 }
 
