b15c566f71fe266864713e4039c29fb70414af9b
qizheng09 <qizheng09@outlook.com>
2019-09-29 14:00:16 +0800

    fix libp2p go.mod and fix badger version

--- vendor/github.com/libp2p/go-libp2p-protocol/protocol.go
+++
@@ -1,9 +0,0 @@
-package protocol
-
-import core "github.com/libp2p/go-libp2p-core/protocol"
-
-// Deprecated: use github.com/libp2p/go-libp2p-core/protocol.ID instead.
-type ID = core.ID
-
-// Deprecated: use github.com/libp2p/go-libp2p-core/protocol.TestingID instead.
-const TestingID = core.TestingID
