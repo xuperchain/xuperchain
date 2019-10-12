6952175828f6612a488967265681bd74a048acb2
Yucao Wang <yucwang@outlook.com>
2019-10-11 22:12:05 -0500

    Add auto loading mechanism of plugins and support system events (#391)

--- common/events/events.go
+++ common/events/events.go
@@ -6,7 +6,15 @@ type EventType uint
 // Definition for internal system events
 const (
 	/* Events for xchain core, start from 1 */
-	// no events now
+	// SystemInitialized is event for system initialized
+	SystemInitialized = 1
+	// SystemStopping is event for system stopping
+	SystemStopping = 2
+
+	// SystemInitialized event for a blockchain is initialized
+	BlockchainInitialized = 3
+	// BlockchainStopping event for a blockchain is stopping
+	BlockchainStopping = 4
 
 	/* Events for consensus, start from 1000 */
 	// ProposerReady current consensus proposers ready for use
