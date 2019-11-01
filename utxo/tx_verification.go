cd8bb553f42a4df36ab55da07a7ea06b14874a31
yucaowang <yucwang@outlook.com>
2019-11-01 22:49:04 +0800

    fix #461: Contract permission check doesn't include initiator

--- utxo/tx_verification.go
+++ utxo/tx_verification.go
@@ -87,6 +87,9 @@ func (uv *UtxoVM) ImmediateVerifyTx(tx *pb.Transaction, isRootTx bool) (bool, er
 			return ok, ErrInvalidSignature
 		}
 
+		// get all authenticated users
+		authUsers := uv.removeDuplicateUser(tx)
+
 		// veify tx UTXO input permission (Account ACL)
 		ok, err = uv.verifyUTXOPermission(tx, verifiedID)
 		if !ok {
@@ -95,7 +98,7 @@ func (uv *UtxoVM) ImmediateVerifyTx(tx *pb.Transaction, isRootTx bool) (bool, er
 		}
 
 		// verify contract requests' permission using ACL
-		ok, err = uv.verifyContractPermission(tx)
+		ok, err = uv.verifyContractPermission(tx, authUsers)
 		if !ok {
 			uv.xlog.Warn("ImmediateVerifyTx: verifyContractPermission failed", "error", err)
 			return ok, ErrACLNotEnough
@@ -404,7 +407,7 @@ func (uv *UtxoVM) verifyRWSetPermission(tx *pb.Transaction, verifiedID map[strin
 }
 
 // verifyContractValid verify the permission of contract requests using ACL
-func (uv *UtxoVM) verifyContractPermission(tx *pb.Transaction) (bool, error) {
+func (uv *UtxoVM) verifyContractPermission(tx *pb.Transaction, allUsers []string) (bool, error) {
 	req := tx.GetContractRequests()
 	if req == nil {
 		// if no contract requests, no need to verify
@@ -416,7 +419,7 @@ func (uv *UtxoVM) verifyContractPermission(tx *pb.Transaction) (bool, error) {
 		contractName := tmpReq.GetContractName()
 		methodName := tmpReq.GetMethodName()
 
-		ok, err := pm.CheckContractMethodPerm(tx.AuthRequire, contractName, methodName, uv.aclMgr)
+		ok, err := pm.CheckContractMethodPerm(allUsers, contractName, methodName, uv.aclMgr)
 		if err != nil || !ok {
 			uv.xlog.Warn("verify contract method ACL failed ", "contract", contractName, "method",
 				methodName, "error", err)
@@ -671,4 +674,22 @@ func (uv *UtxoVM) checkRelyOnMarkedTxid(reftxid []byte, blockid []byte) (bool, b
 		}
 	}
 	return true, isRely, nil
+}
+
+// removeDuplicateUser combine initiator and auth_require and remove duplicate users
+func (uv *UtxoVM) removeDuplicateUser(tx *pb.Transaction) []string {
+	dupCheck := make(map[string]bool)
+	finalUsers := make([]string, 0)
+	if acl.IsAccount(tx.Initiator) == 0 {
+		finalUsers = append(finalUsers, tx.Initiator)
+		dupCheck[tx.Initiator] = true
+	}
+	for _, user := range tx.AuthRequire {
+		if dupCheck[user] {
+			continue
+		}
+		finalUsers = append(finalUsers, user)
+		dupCheck[user] = true
+	}
+	return finalUsers
 }
