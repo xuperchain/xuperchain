0df8b9b5e7150ec9789eafcf0d3e31e28e348c4b
qizheng09 <qizheng09@outlook.com>
2020-02-15 20:16:19 +0800

    add conn close

--- core/p2p/p2pv1/conn.go
+++ core/p2p/p2pv1/conn.go
@@ -86,6 +86,8 @@ func (c *Conn) NewGrpcConn() error {
 func (c *Conn) newClient(ctx context.Context) (p2p_pb.P2PService_SendP2PMessageClient, error) {
 	connState := c.conn.GetState().String()
 	if connState == "TRANSIENT_FAILURE" || connState == "SHUTDOWN" || connState == "Invalid-State" {
+		c.lg.Error("newClient conn state not ready")
+		c.conn.Close()
 		err := c.NewGrpcConn()
 		if err != nil {
 			return nil, err
