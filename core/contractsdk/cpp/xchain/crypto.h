ca39e5204bd4f51a4e34aff0c5e9500628ede889
icexin <fanbingxin.me@gmail.com>
2020-03-14 00:26:45 +0800

    comment change

--- core/contractsdk/cpp/xchain/crypto.h
+++ core/contractsdk/cpp/xchain/crypto.h
@@ -10,7 +10,7 @@ std::string sha256(const std::string& input);
 // hex_encode returns the hex encoding of input
 std::string hex_encode(const std::string& intput);
 // hex_decode returns the hex decoding of input
-// if ret < 0, error happens, otherwise returns the length written
+// if ret false, input is an invalid hex string
 bool hex_decode(const std::string& intput, std::string* output);
 }  // namespace crypto
 }  // namespace xchain
