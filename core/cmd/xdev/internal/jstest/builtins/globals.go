d454c9b0cd64864bfc6302c69a5a0b66299b15d7
fanbingxin <fanbingxin.me@gmail.com>
2020-03-18 17:52:10 +0800

    contractsdk/cpp: add ec verify and make_tx and some tests (#745)
    Add following to cpp sdk:
    
    - Add elliptic curve verification
    - Add txid generate
    - Add tests for crypto
    - Add tests for cross chain
    - Add tests for xuper_relayer contract

--- core/cmd/xdev/internal/jstest/builtins/globals.go
+++ core/cmd/xdev/internal/jstest/builtins/globals.go
@@ -1,10 +1,36 @@
 package builtins
 
+import (
+	"encoding/base64"
+
+	"github.com/robertkrimen/otto"
+)
+
+// Throw throw go error in js vm as an Exception
+func throw(err error) {
+	v, _ := otto.ToValue("Exception: " + err.Error())
+	panic(v)
+}
+
 func bytesToString(s []byte) string {
 	return string(s)
+}
+
+func btoa(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
+
+func atob(s string) string {
+	ret, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		throw(err)
+	}
+	return string(ret)
 }
 
 // Globals will register to global object
 var Globals = map[string]interface{}{
 	"string": bytesToString,
+	"btoa":   btoa,
+	"atob":   atob,
 }
