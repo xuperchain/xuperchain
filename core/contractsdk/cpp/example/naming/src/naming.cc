6e9dda64824a5c42713b74b6c503348a12525d20
fxsjy <ccnusjy@gmail.com>
2020-03-19 12:32:57 +0800

    use xchain::json instead

--- core/contractsdk/cpp/example/naming/src/naming.cc
+++ core/contractsdk/cpp/example/naming/src/naming.cc
@@ -1,5 +1,5 @@
 #include "xchain/xchain.h"
-#include "json.hpp"
+#include "xchain/json/json.h"
 
 #define CHECK_ARG(argKey)                             \
     std::string argKey = ctx->arg(#argKey);           \
@@ -26,10 +26,14 @@ DEFINE_METHOD(Naming, RegisterChain) {
     CHECK_ARG(name);
     CHECK_ARG(type);
     CHECK_ARG(min_endorsor_num);
-    nlohmann::json j;
+    xchain::json j;
     j["type"] = type;
-    j["min_endorsor_num"] = min_endorsor_num;
+    j["min_endorsor_num"] = std::atoi(min_endorsor_num.c_str());
     j["name"] = name;
+    if (j["min_endorsor_num"] <= 0) {
+        ctx->error("invalid min_endorsor_num, it should be greater than 0");
+        return;
+    }
     auto data = j.dump();
     std::string old_data;
     if (ctx->get_object(Meta(name), &old_data)) {
@@ -48,10 +52,14 @@ DEFINE_METHOD(Naming, UpdateChain) {
     CHECK_ARG(name);
     CHECK_ARG(type);
     CHECK_ARG(min_endorsor_num);
-    nlohmann::json j;
+    xchain::json j;
     j["type"] = type;
-    j["min_endorsor_num"] = min_endorsor_num;
+    j["min_endorsor_num"] = std::atoi(min_endorsor_num.c_str());
     j["name"] = name;
+    if (j["min_endorsor_num"] <= 0) {
+        ctx->error("invalid min_endorsor_num, it should be greater than 0");
+        return;
+    }
     auto data = j.dump();
     std::string old_data;
     if (!ctx->get_object(Meta(name), &old_data)) {
@@ -81,11 +89,11 @@ DEFINE_METHOD(Naming, Resolve) {
     CHECK_ARG(name);
     std::unique_ptr<xchain::Iterator> iter =
         ctx->new_iterator(Endorsor(name, ""), Endorsor(name, "~"));
-    nlohmann::json j;
+    xchain::json j;
     while (iter->next()) {
         std::pair<std::string, std::string> kv;
         iter->get(&kv);
-        auto one = nlohmann::json::parse(kv.second);
+        auto one = xchain::json::parse(kv.second);
         j.push_back(one);
     }
     auto result = j.dump();
@@ -103,7 +111,7 @@ DEFINE_METHOD(Naming, AddEndorsor) {
         ctx->error("endorsor already exists");
         return;
     }
-    nlohmann::json j;
+    xchain::json j;
     j["address"] = address;
     j["pub_key"] = pub_key;
     j["host"] = host;
@@ -125,7 +133,7 @@ DEFINE_METHOD(Naming, UpdateEndorsor) {
         ctx->error("endorsor does not exist");
         return;
     }
-    auto j = nlohmann::json::parse(old_info);
+    auto j = xchain::json::parse(old_info);
     j["host"] = host;
     auto info = j.dump();
     if (!ctx->put_object(Endorsor(name, address), info)) {
--- core/contractsdk/cpp/example/naming/test/naming.test.js
+++ core/contractsdk/cpp/example/naming/test/naming.test.js
@@ -12,12 +12,26 @@ Test("naming", function (t) {
         })
     });
 
-    t.Run("invoke", function (tt) {
+    t.Run("Register", function (tt) {
         resp = contract.Invoke("RegisterChain", {"name":"mainnet.xuper","type":"xuper", "min_endorsor_num":"2"});
-	resp2 = contract.Invoke("GetChainMeta", {"name":"mainnet.xuper"})
-	console.log(resp2.Body)
-	obj = JSON.parse(resp2.Body)
+        resp2 = contract.Invoke("GetChainMeta", {"name":"mainnet.xuper"})
+        console.log(resp2.Body)
+        obj = JSON.parse(resp2.Body)
         assert.equal(obj["type"], "xuper") 
-	assert.equal(obj["min_endorsor_num"], "2")
+        assert.equal(obj["min_endorsor_num"], 2)
+        r3 = contract.Invoke("UpdateChain", {"name":"mainnet.xuper", "type":"xuper", "min_endorsor_num":"aaaa"})
+	assert.equal(r3.Message, "invalid min_endorsor_num, it should be greater than 0")
+    })
+
+    t.Run("AddEndorsor", function(tt) {
+        r1 = contract.Invoke("AddEndorsor", {"name":"mainnet.xuper", "address":"bobfffff", "host":"192.168.8.8:37101", "pub_key":"xxxxx"})
+	console.log(r1.Body)
+        r2 = contract.Invoke("AddEndorsor", {"name":"mainnet.xuper", "address":"bobfffff", "host":"192.168.9.9:37101", "pub_key":"xxxxx"})
+	assert.equal(r2.Message, "endorsor already exists")
+	r3 = contract.Invoke("AddEndorsor", {"name":"mainnet.xuper", "address":"alicefff", "host":"192.168.9.9:37101", "pub_key":"xxxxx"})
+	resp = contract.Invoke("Resolve", {"name":"mainnet.xuper"})
+	nodes = JSON.parse(resp.Body)
+	assert.equal(nodes[0].address, "alicefff")
+	assert.equal(nodes[1].address, "bobfffff")
     })
 })
