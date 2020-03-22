aacde8064bab9f0ff3acafcab9ae0e17f61568c3
duanbing <hibduan@gmail.com>
2020-03-22 15:54:24 +0800

    abstract teevm as a plugin

--- build.sh
+++ build.sh
@@ -25,8 +25,6 @@ protoc -I core/contractsdk/pb core/contractsdk/pb/contract.proto \
        --go_out=paths=source_relative:core/contractsdk/go/pb
 protoc -I core/cmd/relayer/pb core/cmd/relayer/pb/relayer.proto \
        --go_out=core/cmd/relayer/pb
-protoc -I core/contract/teevm/pb core/contract/teevm/pb/tf.proto \
-       --go_out=paths=source_relative:core/contract/teevm/pb
 !
 
 # build wasm2c
@@ -35,70 +33,37 @@ cp core/xvm/compile/wabt/build/wasm2c ./
 
 # build framework and tools
 function buildpkg() {
-    args=($1)
-    output=${args[0]}
-    pkg=${args[1]}
-    tags=${args[2]}
-    tag_flags=""
-    if test $tags ;then
-        tag_flags="-tags $tags"
-    fi
-    echo -e "start build $pkg ..."
+    output=$1
+    pkg=$2
     buildVersion=`git rev-parse --abbrev-ref HEAD`
     buildDate=$(date "+%Y-%m-%d-%H:%M:%S")
     commitHash=`git rev-parse --short HEAD`
-    go build $tag_flags -o $output -ldflags "-X main.buildVersion=$buildVersion -X main.buildDate=$buildDate -X main.commitHash=$commitHash" $pkg
+    go build -o $output -ldflags "-X main.buildVersion=$buildVersion -X main.buildDate=$buildDate -X main.commitHash=$commitHash" $pkg
 }
 
-targets=(
- 'xchain-cli github.com/xuperchain/xuperchain/core/cmd/cli'
- 'xchain github.com/xuperchain/xuperchain/core/cmd/xchain'
- 'xdev github.com/xuperchain/xuperchain/core/cmd/xdev'
- 'xchain-httpgw github.com/xuperchain/xuperchain/core/gateway'
- 'dump_chain github.com/xuperchain/xuperchain/core/test'
- 'event_client github.com/xuperchain/xuperchain/core/test/pubsub'
- 'relayer github.com/xuperchain/xuperchain/core/cmd/relayer/relayer'
-)
-
-for i in "${targets[@]}" ; do
-    buildpkg "${i[@]}"
-done
+buildpkg xchain-cli github.com/xuperchain/xuperchain/core/cmd/cli
+buildpkg xchain github.com/xuperchain/xuperchain/core/cmd/xchain
+buildpkg xdev github.com/xuperchain/xuperchain/core/cmd/xdev
+buildpkg xchain-httpgw github.com/xuperchain/xuperchain/core/gateway
+buildpkg dump_chain github.com/xuperchain/xuperchain/core/test
+buildpkg event_client github.com/xuperchain/xuperchain/core/test/pubsub
+buildpkg relayer github.com/xuperchain/xuperchain/core/cmd/relayer/relayer
 
 # build plugins
 echo "OS:"${PLATFORM}
 echo "## Build Plugins..."
 mkdir -p core/plugins/kv core/plugins/crypto core/plugins/consensus core/plugins/contract
-
-# build plugins 
-function build_plugin() {
-    args=($1)
-    target=${args[0]}
-    src=${args[1]}
-    tags=${args[2]}
-    tag_flags=""
-    if test $tags ;then
-        tag_flags="-tags $tags" 
-    fi 
-    go build --buildmode=plugin ${tag_flags} -o $target $src 
-}
-
-plugins=(
-  'core/plugins/kv/kv-ldb-multi.so.1.0.0 github.com/xuperchain/xuperchain/core/kv/kvdb/plugin-ldb  multi'
-  'core/plugins/kv/kv-ldb-single.so.1.0.0 github.com/xuperchain/xuperchain/core/kv/kvdb/plugin-ldb single'
-  'core/plugins/kv/kv-badger.so.1.0.0 github.com/xuperchain/xuperchain/core/kv/kvdb/plugin-badger'
-  'core/plugins/crypto/crypto-default.so.1.0.0 github.com/xuperchain/xuperchain/core/crypto/client/xchain/plugin_impl'
-  'core/plugins/crypto/crypto-schnorr.so.1.0.0 github.com/xuperchain/xuperchain/core/crypto/client/schnorr/plugin_impl'
-  'core/plugins/crypto/crypto-gm.so.1.0.0 github.com/xuperchain/xuperchain/core/crypto/client/gm/gmclient/plugin_impl'
-  'core/plugins/consensus/consensus-pow.so.1.0.0 github.com/xuperchain/xuperchain/core/consensus/pow'
-  'core/plugins/consensus/consensus-single.so.1.0.0 github.com/xuperchain/xuperchain/core/consensus/single'
-  'core/plugins/consensus/consensus-tdpos.so.1.0.0 github.com/xuperchain/xuperchain/core/consensus/tdpos/main'
-  'core/plugins/p2p/p2p-p2pv1.so.1.0.0 github.com/xuperchain/xuperchain/core/p2p/p2pv1/plugin_impl'
-  'core/plugins/p2p/p2p-p2pv2.so.1.0.0 github.com/xuperchain/xuperchain/core/p2p/p2pv2/plugin_impl'
-)
-
-for i in "${plugins[@]}" ; do
-    build_plugin "${i[@]}"
-done
+go build --buildmode=plugin --tags multi -o core/plugins/kv/kv-ldb-multi.so.1.0.0 github.com/xuperchain/xuperchain/core/kv/kvdb/plugin-ldb
+go build --buildmode=plugin --tags single -o core/plugins/kv/kv-ldb-single.so.1.0.0 github.com/xuperchain/xuperchain/core/kv/kvdb/plugin-ldb
+go build --buildmode=plugin -o core/plugins/kv/kv-badger.so.1.0.0 github.com/xuperchain/xuperchain/core/kv/kvdb/plugin-badger
+go build --buildmode=plugin -o core/plugins/crypto/crypto-default.so.1.0.0 github.com/xuperchain/xuperchain/core/crypto/client/xchain/plugin_impl
+go build --buildmode=plugin -o core/plugins/crypto/crypto-schnorr.so.1.0.0 github.com/xuperchain/xuperchain/core/crypto/client/schnorr/plugin_impl
+go build --buildmode=plugin -o core/plugins/crypto/crypto-gm.so.1.0.0 github.com/xuperchain/xuperchain/core/crypto/client/gm/gmclient/plugin_impl
+go build --buildmode=plugin -o core/plugins/consensus/consensus-pow.so.1.0.0 github.com/xuperchain/xuperchain/core/consensus/pow
+go build --buildmode=plugin -o core/plugins/consensus/consensus-single.so.1.0.0 github.com/xuperchain/xuperchain/core/consensus/single
+go build --buildmode=plugin -o core/plugins/consensus/consensus-tdpos.so.1.0.0 github.com/xuperchain/xuperchain/core/consensus/tdpos/main
+go build --buildmode=plugin -o core/plugins/p2p/p2p-p2pv1.so.1.0.0 github.com/xuperchain/xuperchain/core/p2p/p2pv1/plugin_impl
+go build --buildmode=plugin -o core/plugins/p2p/p2p-p2pv2.so.1.0.0 github.com/xuperchain/xuperchain/core/p2p/p2pv2/plugin_impl
 
 
 # build output dir
