c68074d5018778be2a76543da566290b16280af5
fxsjy <ccnusjy@gmail.com>
2020-03-25 13:20:28 +0800

    make tx reposting more efficent

--- core/utxo/topsort.go
+++ core/utxo/topsort.go
@@ -1,6 +1,6 @@
 package utxo
 
-// TxGraph 交易依赖关系图
+//交易依赖关系图
 type TxGraph map[string][]string
 
 /*
@@ -8,28 +8,23 @@ type TxGraph map[string][]string
 *  'tx3' --> ['tx1', 'tx2']  tx3依赖了tx1,tx2, 也可以表示反向依赖关系:tx3被tx1,tx2依赖
 *  'tx2' --> ['tx0', 'tx1']
  */
-
-// TopSortDFS 对依赖关系图进行拓扑排序
+//对依赖关系图进行拓扑排序
 // 输入：依赖关系图，就是个map
 // 输出: order: 排序后的有序数组，依赖者排在前面，被依赖的排在后面
 //       cyclic: 如果发现有环形依赖关系则输出这个数组
 //
 // 实现参考： https://rosettacode.org/wiki/Topological_sort#Go
-func TopSortDFS(g TxGraph) (order []string, cycle bool, childDAGsSize []int) {
-	// 统计每个tx的次数(包括被引用以及引用次数)
-	degreeForTx := map[string]int{}
-	headTx := map[string]int{}
-	cyclic := []string{}
-	for k, outputs := range g {
-		degreeForTx[k]++
-		headTx[k]++
+func TopSortDFS(g TxGraph) (order []string, cyclic bool, childDAGSize []int) {
+	reverseG := TxGraph{}
+	for n, outputs := range g {
 		for _, m := range outputs {
-			headTx[m]--
 			if g[m] == nil {
 				g[m] = []string{} //预处理一下，coinbase交易可能没有依赖
-				degreeForTx[m]++
 			}
-			degreeForTx[m]++
+			if reverseG[m] == nil {
+				reverseG[m] = []string{}
+			}
+			reverseG[m] = append(reverseG[m], n)
 		}
 	}
 	L := make([]string, len(g))
@@ -37,13 +32,11 @@ func TopSortDFS(g TxGraph) (order []string, cycle bool, childDAGsSize []int) {
 	temp := map[string]bool{} //临时访问标记
 	perm := map[string]bool{} //永久访问标记
 	var cycleFound bool
-	var cycleStart string
 	var visit func(string)
 	visit = func(n string) {
 		switch {
 		case temp[n]: //临时标记里面有，说明产生环了
 			cycleFound = true
-			cycleStart = n
 			return
 		case perm[n]:
 			return
@@ -52,12 +45,7 @@ func TopSortDFS(g TxGraph) (order []string, cycle bool, childDAGsSize []int) {
 		for _, m := range g[n] {
 			visit(m)
 			if cycleFound {
-				if cycleStart > "" {
-					cyclic = append(cyclic, n)
-					if n == cycleStart {
-						cycleStart = ""
-					}
-				}
+				cyclic = true
 				return
 			}
 		}
@@ -66,48 +54,44 @@ func TopSortDFS(g TxGraph) (order []string, cycle bool, childDAGsSize []int) {
 		i--
 		L[i] = n
 	}
-	// 上一个子DAG对应的起始数组索引
-	lastDAGIdx := len(L)
-	// 当前子DAG对应的tx个数
-	currChildDAGSize := 0
+	subGraphs := [][]string{}
+	marked := map[string]bool{}
+	subG := []string{}
+	var dfs func(string)
+	dfs = func(n string) {
+		if marked[n] {
+			return
+		}
+		marked[n] = true
+		for _, m := range g[n] {
+			dfs(m)
+		}
+		for _, m := range reverseG[n] {
+			dfs(m)
+		}
+		subG = append(subG, n)
+	}
 	for n := range g {
-		if perm[n] || len(g[n]) <= 0 {
+		if marked[n] {
 			continue
 		}
-		if v, ok := headTx[n]; ok && v != 1 {
-			continue
-		}
-		visit(n)
-
-		currChildDAGSize = lastDAGIdx - i
-		childDAGsSize = append(childDAGsSize, currChildDAGSize)
-		lastDAGIdx = i
-
-		if cycleFound {
-			return nil, true, reverse(childDAGsSize)
-		}
+		dfs(n)
+		subGraphs = append(subGraphs, subG)
+		subG = []string{}
 	}
-	leftIdx := 0
-	for k, _ := range degreeForTx {
-		// 将入度和出度为0的tx分离出来
-		// degreeForTx[k] == 1表示tx入度为0
-		// len(g[k])表示tx的出度为0
-		if degreeForTx[k] == 1 && len(g[k]) <= 0 {
-			L[leftIdx] = k
-			leftIdx++
-			childDAGsSize = append(childDAGsSize, 1)
+
+	childDAGSize = make([]int, len(subGraphs))
+	for i, g := range subGraphs {
+		childDAGSize[len(subGraphs)-i-1] = len(g)
+		for _, n := range g {
+			if perm[n] {
+				continue
+			}
+			visit(n)
+			if cycleFound {
+				return nil, cyclic, childDAGSize
+			}
 		}
 	}
-	// 存在环
-	if leftIdx != i {
-		return nil, true, nil
-	}
-	return L, false, reverse(childDAGsSize)
-}
-
-func reverse(list []int) []int {
-	for i, j := 0, len(list)-1; i < j; i, j = i+1, j-1 {
-		list[i], list[j] = list[j], list[i]
-	}
-	return list
+	return L, false, childDAGSize
 }
--- core/utxo/topsort_test.go
+++ core/utxo/topsort_test.go
@@ -101,7 +101,6 @@ func TestTopSortWithoutCircleCase1(t *testing.T) {
 		for _, v := range currChildStr {
 			tmpStr += v
 		}
-
 		childDAGSlice = append(childDAGSlice, tmpStr)
 		start = end
 		idx++
@@ -126,30 +125,6 @@ func TestTopSortWithoutCircleCase2(t *testing.T) {
 	if circle || len(order) != 6 || len(childDAG) != 2 {
 		t.Error("TestTopSortWithoutCircle error")
 	}
-	childDAGSlice := []string{}
-	// 按照childDAG拆分出多个子DAG字符串
-	idx := 0
-	start, end, length := 0, 0, len(childDAG)
-	for idx < length {
-		end += childDAG[idx]
-		currChildStr := getStr(start, end, order)
-		tmpStr := ""
-		for _, v := range currChildStr {
-			tmpStr += v
-		}
-
-		childDAGSlice = append(childDAGSlice, tmpStr)
-		start = end
-		idx++
-	}
-	sort.Strings(childDAGSlice)
-	orderStr := ""
-	for _, str := range childDAGSlice {
-		orderStr += str
-	}
-	if orderStr != "tx3tx2tx1tx4tx6tx5" {
-		t.Error("TestTopSortWithoutCircle error")
-	}
 }
 
 // 无环 + case3
@@ -165,32 +140,20 @@ func TestTopSortWithoutCircleCase3(t *testing.T) {
 	t.Log("order->", order)
 	t.Log("circle->", circle)
 	t.Log("childDAG->", childDAG)
-	childDAGSlice := []string{}
-	// 按照childDAG拆分出多个子DAG字符串
-	idx := 0
-	start, end, length := 0, 0, len(childDAG)
-	for idx < length {
-		end = start + childDAG[idx]
-		currChildStr := getStr(start, end, order)
-		tmpStr := ""
-		for _, v := range currChildStr {
-			tmpStr += v
-		}
-
-		childDAGSlice = append(childDAGSlice, tmpStr)
-		start = end
-		idx++
+	oo := map[string]int{}
+	for idx, tx := range order {
+		oo[tx] = idx
 	}
-	sort.Strings(childDAGSlice)
-	orderStr := ""
-	for _, str := range childDAGSlice {
-		orderStr += str
-	}
-	t.Log("orderStr->", orderStr)
-	if orderStr != "tx3tx2tx1tx4tx6tx5tx7" {
-		t.Error("TestTopSortWithoutCircle error")
+	if oo["tx3"] < oo["tx1"] &&
+		oo["tx3"] < oo["tx2"] &&
+		oo["tx4"] < oo["tx5"] &&
+		oo["tx4"] < oo["tx6"] {
+		t.Log("order ok")
+	} else {
+		t.Error("order check failed at TestTopSortWithoutCircleCase3")
 	}
 }
+
 func getStr(start int, end int, order []string) []string {
 	fmt.Println("getStr->", "start->", start, " end->", end)
 	ret := []string{}
@@ -199,4 +162,21 @@ func getStr(start int, end int, order []string) []string {
 	}
 
 	return ret
+}
+
+func TestTopSortWithoutCircleCase4(t *testing.T) {
+	graph := map[string][]string{}
+	graph["tx1"] = []string{"tx2"}
+	graph["tx2"] = []string{"tx3"}
+	graph["tx3"] = []string{"tx4"}
+	graph["tx4"] = []string{"tx5"}
+	graph["tx6"] = []string{"tx3"}
+
+	order, circle, childDAG := TopSortDFS(graph)
+	if circle || len(order) != 6 || len(childDAG) != 1 {
+		t.Error("TestTopSortWithoutCircle error")
+	}
+	t.Log("order->", order)
+	t.Log("circle->", circle)
+	t.Log("childDAG->", childDAG)
 }
