74ed862ccde166795e12ee36ebdf5f28f46a5c43
Hongyan Wang <hongyan.wang@ucalgary.ca>
2020-04-16 21:29:49 +0800

    Update trust_operators.h

--- core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
+++ core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
@@ -8,12 +8,9 @@ class TrustOperators {
 public:
     TrustOperators(xchain::Context*, const uint32_t);
 
-    std::string add(const std::string left_value, const std::string right_value, const std::string output_key);
-    std::string sub(const std::string left_value, const std::string right_value, const std::string output_key);
-    std::string mul(const std::string left_value, const std::string right_value, const std::string output_key);
+    bool ops(const std::string op, const std::string left_value, const std::string right_value, const std::string output_key);
    
 private:
     xchain::Context* _ctx;
     const uint32_t _svn;
 };
-
