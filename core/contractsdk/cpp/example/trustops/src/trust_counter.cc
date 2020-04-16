cd7798a6be2168c2542eb772a2b75bac6b7603c2
Hongyan Wang <hongyan.wang@ucalgary.ca>
2020-04-16 21:28:54 +0800

    Update trust_counter.cc

--- core/contractsdk/cpp/example/trustops/src/trust_counter.cc
+++ core/contractsdk/cpp/example/trustops/src/trust_counter.cc
@@ -16,6 +16,7 @@ DEFINE_METHOD(Counter, initialize) {
     ctx->ok("initialize succeed");
 }
 
+// get a number by key
 DEFINE_METHOD(Counter, get) {
     xchain::Context* ctx = self.context();
     const std::string& key = ctx->arg("key");
@@ -27,16 +28,20 @@ DEFINE_METHOD(Counter, get) {
     }
 }
 
+// store saves encrypted data directly
 DEFINE_METHOD(Counter, store) {
     xchain::Context* ctx = self.context();
+    std::string debug = "done";
     for(auto it = ctx->args().begin(); it != ctx->args().end(); ++it) {
         // args are already encrypted, just put
-        ctx->put_object(it->first, it->second);
+        auto ok = ctx->put_object(it->first, it->second);
+        if (!ok) { debug = "error"; }
     }
-
-    ctx->ok("done");
+    ctx->ok(debug);
 }
 
+// add adds two encrypted numbers
+// input is in format {"l":"key_l", "r":"key_r", "o":"key_output"}
 DEFINE_METHOD(Counter, add) {
     xchain::Context* ctx = self.context();
     TrustOperators to(ctx, 0);
@@ -53,10 +58,14 @@ DEFINE_METHOD(Counter, add) {
         }
     }
 
-    auto ok = to.add(argsMap["l"], argsMap["r"], argsMap["o"]);
-    ctx->ok(ok);
+    auto ok = to.ops("add", argsMap["l"], argsMap["r"], argsMap["o"]);
+    std::string debug = "done";
+    if (!ok) { debug = "error"; }
+    ctx->ok(debug);
 }
 
+// sub substracts one number from another
+// input is in format {"l":"key_l", "r":"key_r", "o":"key_output"}
 DEFINE_METHOD(Counter, sub) {
    xchain::Context* ctx = self.context();
     TrustOperators to(ctx, 0);
@@ -73,11 +82,14 @@ DEFINE_METHOD(Counter, sub) {
         }
     }
 
-    auto ok = to.sub(argsMap["l"], argsMap["r"], argsMap["o"]);
-    ctx->ok(ok);
+    auto ok = to.ops("sub", argsMap["l"], argsMap["r"], argsMap["o"]);
+    std::string debug = "done";
+    if (!ok) { debug = "error"; }
+    ctx->ok(debug);
 }
 
-
+// mul multiplies two encrypted numbers
+// input is in format {"l":"key_l", "r":"key_r", "o":"key_output"}
 DEFINE_METHOD(Counter, mul) {
    xchain::Context* ctx = self.context();
     TrustOperators to(ctx, 0);
@@ -94,6 +106,8 @@ DEFINE_METHOD(Counter, mul) {
         }
     }
 
-    auto ok = to.mul(argsMap["l"], argsMap["r"], argsMap["o"]);
-    ctx->ok(ok);
+    auto ok = to.ops("mul", argsMap["l"], argsMap["r"], argsMap["o"]);
+    std::string debug = "done";
+    if (!ok) { debug = "error"; }
+    ctx->ok(debug);
 }
