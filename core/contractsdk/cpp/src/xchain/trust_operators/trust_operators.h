92145e9523b5d6c021788ae12c3c0d5d6392f9c4
Hongyan Wang <hongyan.wang@ucalgary.ca>
2020-04-17 10:57:38 +0800

    Update trust_operators.h

--- core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
+++ core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
@@ -1,20 +1,24 @@
 #pragma once
+#include "xchain/xchain.h"
 #include <map>
 #include <string>
-#include "xchain/xchain.h"
-
 
 class TrustOperators {
 public:
-    TrustOperators(xchain::Context*, const uint32_t);
+  TrustOperators(xchain::Context *, const uint32_t);
 
-    bool add(const std::string left_value, const std::string right_value, const std::string output_key);
-    bool sub(const std::string left_value, const std::string right_value, const std::string output_key);
-    bool mul(const std::string left_value, const std::string right_value, const std::string output_key);
-   
+  bool add(const std::string &left_value, const std::string &right_value,
+           const std::string &output_key);
+  bool sub(const std::string &left_value, const std::string &right_value,
+           const std::string &output_key);
+  bool mul(const std::string &left_value, const std::string &right_value,
+           const std::string &output_key);
+
 private:
-    xchain::Context* _ctx;
-    const uint32_t _svn;
+  xchain::Context *_ctx;
+  const uint32_t _svn;
 
-    bool binary_ops(const std::string op, const std::string left_value, const std::string right_value, const std::string output_key);
+  bool binary_ops(const std::string op, const std::string &left_value,
+                  const std::string &right_value,
+                  const std::string &output_key);
 };
