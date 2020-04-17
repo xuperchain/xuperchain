6b5e2915ec6e030a5f78df4ba05db913f4618385
Hongyan Wang <hongyan.wang@ucalgary.ca>
2020-04-17 20:38:17 +0800

    trust_operators modification (#793)
    add binary operations(add, sub, mul) for trust_operators, delete "store" operation.

--- core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
+++ core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
@@ -1,15 +1,24 @@
 #pragma once
+#include "xchain/xchain.h"
 #include <map>
 #include <string>
-#include "xchain/xchain.h"
-
 
 class TrustOperators {
 public:
-    TrustOperators(const std::string&);
-    bool store(xchain::Context* ctx, const uint32_t svn, const std::string& args);	
-   
-private: 
-    const std::string& _address;
-};
+  TrustOperators(xchain::Context *, const uint32_t);
 
+  bool add(const std::string &left_value, const std::string &right_value,
+           const std::string &output_key);
+  bool sub(const std::string &left_value, const std::string &right_value,
+           const std::string &output_key);
+  bool mul(const std::string &left_value, const std::string &right_value,
+           const std::string &output_key);
+
+private:
+  xchain::Context *_ctx;
+  const uint32_t _svn;
+
+  bool binary_ops(const std::string op, const std::string &left_value,
+                  const std::string &right_value,
+                  const std::string &output_key);
+};
