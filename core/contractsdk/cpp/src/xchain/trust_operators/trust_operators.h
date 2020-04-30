48bb902277751c3185c14ec396f074668a5dac45
Wang <hongyan.wang@ucalgary.ca>
2020-04-30 17:22:16 +0800

    add data authorization contract

--- core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
+++ core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
@@ -7,18 +7,34 @@ class TrustOperators {
 public:
   TrustOperators(xchain::Context *, const uint32_t);
 
-  bool add(const std::string &left_value, const std::string &right_value,
-           const std::string &output_key);
-  bool sub(const std::string &left_value, const std::string &right_value,
-           const std::string &output_key);
-  bool mul(const std::string &left_value, const std::string &right_value,
-           const std::string &output_key);
+  struct Operand {
+    std::string cipher;
+    std::string commitment;
+  };
+
+  struct AuthInfo {
+    std::string data;
+    std::string to;
+    std::string pubkey;
+    std::string signature;
+    std::string kind;
+  };
+
+  bool add(const Operand &left_op, const Operand &right_op,
+           std::map<std::string, std::string> *result);
+  bool sub(const Operand &left_op, const Operand &right_op,
+           std::map<std::string, std::string> *result);
+  bool mul(const Operand &left_op, const Operand &right_op,
+           std::map<std::string, std::string> *result);
+
+  bool authorize(const AuthInfo &auth,
+                 std::map<std::string, std::string> *result);
 
 private:
   xchain::Context *_ctx;
   const uint32_t _svn;
 
-  bool binary_ops(const std::string op, const std::string &left_value,
-                  const std::string &right_value,
-                  const std::string &output_key);
+  bool binary_ops(const std::string op, const Operand &left_op,
+                  const Operand &right_op,
+                  std::map<std::string, std::string> *result);
 };
