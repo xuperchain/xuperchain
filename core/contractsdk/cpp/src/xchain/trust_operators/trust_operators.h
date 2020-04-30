c44e57d5109a57521762e728bdbfcc275d9e2c14
Wang <hongyan.wang@ucalgary.ca>
2020-04-30 19:41:35 +0800

    add comments

--- core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
+++ core/contractsdk/cpp/src/xchain/trust_operators/trust_operators.h
@@ -8,16 +8,17 @@ public:
   TrustOperators(xchain::Context *, const uint32_t);
 
   struct Operand {
-    std::string cipher;
-    std::string commitment;
+    std::string cipher;     //ciphertext
+    std::string commitment; //user's proof of right to use data
   };
 
+  // information for authorization request
   struct AuthInfo {
-    std::string data;
-    std::string to;
-    std::string pubkey;
-    std::string signature;
-    std::string kind;
+    std::string data;       //data content
+    std::string to;         //user address to be authorized
+    std::string pubkey;     //public key of owner
+    std::string signature;  //owner's signature for this authorization request
+    std::string kind;       //"commitment" for data usage, "ownership" for data ownership
   };
 
   bool add(const Operand &left_op, const Operand &right_op,
