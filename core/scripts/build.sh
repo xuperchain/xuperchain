001b9ec04fc8339b97fe32a33b85ce20aa79bab6
limeng120 <1625272999@qq.com>
2020-06-11 18:09:36 +0800

    add relayer.yaml to output

--- core/scripts/build.sh
+++ core/scripts/build.sh
@@ -56,5 +56,6 @@ mv relayer ${output_dir}
 cp -rf core/plugins ${output_dir}
 cp -rf core/data ${output_dir}
 cp -rf core/conf ${output_dir}
+cp -rf core/cmd/relayer/conf/relayer.yaml ${output_dir}/conf
 cp -rf core/cmd/quick_shell/* ${output_dir}
 mkdir -p ${output_dir}/data/blockchain
