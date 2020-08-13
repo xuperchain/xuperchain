abfcf3a3c5dade5687e4c2bf696ef786d345a3aa
aucusaga <aucusaga@hotmail.com>
2020-08-13 13:48:17 +0800

    xpoa-bug-fix-list & xpoa-opti: add unit test

--- core/consensus/common/chainedbft/pacemaker_interface.go
+++ core/consensus/common/chainedbft/pacemaker_interface.go
@@ -27,7 +27,8 @@ type PacemakerInterface interface {
 	UpdateSmrState(generateQC *pb.QuorumCert)
 	// IsLastViewConfirmed check if last block is confirmed
 	IsLastViewConfirmed() (bool, error)
-	CheckViewNumer(viewNumber int64) bool 
+	// CheckViewNumer check if the viewNumber given is valid in smr's viewMsgs storage
+	CheckViewNumer(viewNumber int64) bool
 	GetChainedBFT() *ChainedBft
 	Start() error
 	Stop() error
