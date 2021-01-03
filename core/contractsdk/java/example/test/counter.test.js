df36ec5fea44832b9f52e7d2178d8021d9ab26de
chenfengjin <1871653365@qq.coom>
2021-01-04 05:03:06 +0800

    RFC: Add award manage

--- core/contractsdk/java/example/test/counter.test.js
+++ core/contractsdk/java/example/test/counter.test.js
@@ -1,28 +1,28 @@
 var assert = require("assert");
 
-var codePath="../counter/target/counter-0.1.0-jar-with-dependencies.jar"
+var codePath = "../counter/target/counter-0.1.0-jar-with-dependencies.jar"
 
-var lang="java"
-var type="native"
+var lang = "java"
+var type = "native"
 function deploy() {
     return xchain.Deploy({
         name: "counter",
         code: codePath,
         lang: lang,
-        type:type,
-        init_args: {"creator":"xchain"}
+        type: type,
+        init_args: { "creator": "xchain" }
     });
 }
 
 Test("Increase", function (t) {
     var c = deploy();
-var resp = c.Invoke("increase",{"key":"xchain"},{"name":"11111"});
+    var resp = c.Invoke("increase", { "key": "xchain" }, { "name": "11111" });
     assert.equal(resp.Body, "1");
 })
 
-Test("Get",function (t) {
+Test("Get", function (t) {
     var c = deploy()
-var resp = c.Invoke("increase",{"key":"xchain"});
-var resp = c.Invoke("get",{"key":"xchain"})
-    assert.equal(resp.Body,"1")
+    var resp = c.Invoke("increase", { "key": "xchain" });
+    var resp = c.Invoke("get", { "key": "xchain" })
+    assert.equal(resp.Body, "1")
 })
