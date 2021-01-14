35e19e950f405a2605f7cdb094487a4b6a950a1b
Tri-stone <njustzhangl@163.com>
2021-01-14 11:05:01 +0800

    [fix]cmd/cli: response of evm with endorsing service (#974)

--- core/cmd/cli/comm_trans.go
+++ core/cmd/cli/comm_trans.go
@@ -148,7 +148,7 @@ func (c *CommTrans) GenPreExeRes(ctx context.Context) (
 			// print contract response of evm
 			err := printRespWithAbiForEVM(string(c.AbiCode), c.MethodName, res.Body)
 			if err != nil {
-				return nil, nil, nil
+				fmt.Printf("contract response: %s\n", string(res.Body))
 			}
 		}
 	}
