cce4bf15ad4fc3bf3ad8865d7163e78ac0358c00
Go语言之美 <rayforce.lee@gmail.com>
2021-02-07 15:00:24 +0800

    feat: evm abi encode on the server (#995)
    xchain server can do abi encode when solidity contract deploy or invoke.

--- core/cmd/cli/contract_query.go
+++ core/cmd/cli/contract_query.go
@@ -84,14 +84,13 @@ func (c *ContractQueryCommand) query(ctx context.Context, codeName string) error
 	if err != nil {
 		return err
 	}
+
 	if c.module == string(bridge.TypeEvm) {
-		ct.Args, ct.AbiCode, err = convertToEvmArgsWithAbiFile(c.abiFile, c.methodName, args)
-		if err != nil {
+		if ct.Args, err = convertToXuper3EvmArgs(args); err != nil {
 			return err
 		}
 	} else {
-		ct.Args, err = convertToXuper3Args(args)
-		if err != nil {
+		if ct.Args, err = convertToXuper3Args(args); err != nil {
 			return err
 		}
 	}
