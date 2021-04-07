afddcb3659e5e16bc57a97f5f3090fcd84dac43b
chenfengjin <1871653365@qq.coom>
2021-04-07 12:02:57 +0800

    RFC: Remove useless code

--- core/scripts/build.sh
+++ core/scripts/build.sh
@@ -14,13 +14,12 @@ fi
 
 # build framework and tools
 function buildpkg() {
-        output=$1
-        pkg=$2
-        buildDate=$(date "+%Y-%m-%d-%H:%M:%S")
-        commitHash=$(git rev-parse --short HEAD 2>/dev/null || echo unknown)
-        buildVersion=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo unknown)
-
-        go build -o $output -gcflags "${XCHAIN_CUSTOME_BUILD_GCFLAGS}" -ldflags "-X main.buildVersion=$buildVersion -X main.buildDate=$buildDate -X main.commitHash=$commitHash" $pkg
+    output=$1
+    pkg=$2
+    buildDate=$(date "+%Y-%m-%d-%H:%M:%S")
+    commitHash=$(git rev-parse --short HEAD 2>/dev/null || echo unknown)
+    buildVersion=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo unknown)
+    go build -o $output -gcflags "${XCHAIN_CUSTOME_BUILD_GCFLAGS}" -ldflags "-X main.buildVersion=$buildVersion -X main.buildDate=$buildDate -X main.commitHash=$commitHash" $pkg
 }
 
 buildpkg xchain-cli github.com/xuperchain/xuperchain/core/cmd/cli
